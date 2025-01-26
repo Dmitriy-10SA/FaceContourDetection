e125e96373f845ca1ed9f819ba8c12b5a7f1f969
Дмитрий Семкин <semkin_dmitriy10@vk.com>
2025-01-27 00:28:34 +0400

    Permissions situation

--- app/src/main/java/com/andef/facecontourdetection/MainActivity.kt
+++ app/src/main/java/com/andef/facecontourdetection/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.andef.facecontourdetection
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -27,5 +35,59 @@ class MainActivity : AppCompatActivity() {
         overlayView = findViewById(R.id.overlayView)
 
         floatingActionButtonUpheaval = findViewById(R.id.floatingActionButtonUpheaval)
+
+        val permissionStatus = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        )
+
+        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+            //
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_CODE_PERMISSION_CAMERA
+            )
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE_PERMISSION_CAMERA -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //
+                } else {
+                    showSettingsDialog()
+                }
+            }
+        }
+    }
+
+    private fun showSettingsDialog() {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.permission_is_required)
+            .setMessage(R.string.you_can_grant_permissionuse_camera_app_settings)
+            .setPositiveButton(R.string.open_settings) {_, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            .setNegativeButton(R.string.cancellation) {dialog, _ ->
+                dialog.dismiss()
+                finish()
+            }
+            .create()
+            .show()
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSION_CAMERA = 1
     }
 }
