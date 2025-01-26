ac89a09d35625a1b9c7bb300f1f595d9d1fdf438
Дмитрий Семкин <semkin_dmitriy10@vk.com>
2025-01-27 01:17:15 +0400

    Init overlayView

--- app/src/main/java/com/andef/facecontourdetection/OverlayView.kt
+++ app/src/main/java/com/andef/facecontourdetection/OverlayView.kt
@@ -1,9 +1,46 @@
 package com.andef.facecontourdetection
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import com.google.mlkit.vision.face.Face
 
-class OverlayView(context: Context, attrs: AttributeSet?): View(context, attrs) {
+class OverlayView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+    private val paint = Paint().apply {
+        color = Color.GREEN
+        style = Paint.Style.STROKE
+        strokeWidth = 8f
+    }
 
+    private var _faces = listOf<Face>()
+    var faces = _faces.toList()
+        get() = _faces.toList()
+        set(value) {
+            _faces = value
+            field = _faces.toList()
+            invalidate()
+        }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        if (_faces.isNotEmpty()) {
+            for (face in _faces) {
+                val faceContour = face.allContours[0]
+                val points = faceContour.points
+                var startX = points[0].x
+                var startY = points[0].y
+                for (point in points) {
+                    val finishX = point.x
+                    val finishY = point.y
+                    canvas.drawLine(startX, startY, finishX, finishY, paint)
+                    startX = finishX
+                    startY = finishY
+                }
+                canvas.drawLine(startX, startY, points[0].x, points[0].y, paint)
+            }
+        }
+    }
 }
