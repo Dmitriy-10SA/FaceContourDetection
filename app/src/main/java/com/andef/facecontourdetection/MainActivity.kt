37e3703acc9f6fa71aae0460e4c2f6e1a76f3ab3
Дмитрий Семкин <semkin_dmitriy10@vk.com>
2025-01-27 00:16:36 +0400

    Main screen and overlayView

--- app/src/main/java/com/andef/facecontourdetection/MainActivity.kt
+++ app/src/main/java/com/andef/facecontourdetection/MainActivity.kt
@@ -3,18 +3,29 @@ package com.andef.facecontourdetection
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.view.PreviewView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var previewView: PreviewView
+    private lateinit var overlayView: OverlayView
+
+    private lateinit var floatingActionButtonUpheaval: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        previewView = findViewById(R.id.previewView)
+        overlayView = findViewById(R.id.overlayView)
+
+        floatingActionButtonUpheaval = findViewById(R.id.floatingActionButtonUpheaval)
     }
 }
---
+++ app/src/main/java/com/andef/facecontourdetection/OverlayView.kt
@@ -0,0 +1,9 @@
+package com.andef.facecontourdetection
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class OverlayView(context: Context, attrs: AttributeSet?): View(context, attrs) {
+
+}
